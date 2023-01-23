9fd44e5efb475420ac2debe8027c8a86dfc06e2f
otomomik <otomomik@rainri.com>
2023-01-23 20:47:46 +0900

    nextのバージョン修正

--- src/client/trpc.ts
+++ src/client/trpc.ts
@@ -4,8 +4,8 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/api/trpc",
-      // url: "https://next-trpc.pages.dev/api/trpc",
+      // url: "http://localhost:3000/api/trpc",
+      url: "https://next-trpc.pages.dev/api/trpc",
     }),
   ],
 });
